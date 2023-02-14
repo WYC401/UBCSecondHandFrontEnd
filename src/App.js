6c534d42c266ccb1a14aa9b190987319b042f100
WYC401 <545301030@qq.com>
2023-02-13 23:16:52 -0800

    kind of finish all the desired part in static web

--- src/App.js
+++ src/App.js
@@ -3,16 +3,47 @@ import './App.css';
 import SideMenu from"./components/SideMenu.js";
 import UpperMenue from './components/UpperMenue';
 import ItemBox from './components/ItemBox';
-function App() {
-  return (
-    <div className='wholeBackground'>
-    <UpperMenue/>
-    <div  className="downBackground">
-      <SideMenu/>
-      <div className="main"><ItemBox imgPath="./images/ubc-logo.png" title="Road Bike" category="Bike" description="this is a bike"/></div>
-    </div>
-    </div>
-  );
+import React, { Component } from 'react';
+
+
+
+export default class App extends React.Component {
+
+  render() {
+    const itemList = [
+      {
+        id:"1",
+        imgPath: "https://images2.giant-bicycles.com/b_white,c_pad,h_850,q_80/pbbsrk9upf0yug7yp3oa/MY22ATX27-5_ColorBVibrantBlue.jpg",
+        title: "Road Bike",
+        price: "20",
+        category: "bike",
+        description: "This is a bike"
+      },
+      {
+        id: "2",
+        imgPath: "https://www.91-img.com/gallery_images_uploads/d/7/d7cf5e2b1a3a3dfcca8a8dbb524fb11a8fb1c8e8.JPG",
+        title: "Use Iphone",
+        price: "100",
+        category: "cell phone",
+        description: "a 95% new iphone only used for 2 months"
+      }
+    ]
+    console.log(itemList);
+    const ItemBoxList = itemList.map((item)=>{
+      return <li key={item.id}>{<ItemBox imgPath={item.imgPath} title={item.title} price={item.price} category={item.category} description={item.description}/>}</li>
+    });
+    console.log(ItemBoxList);
+    return (
+      <div className='wholeBackground'>
+      <UpperMenue/>
+      <div  className="downBackground">
+        <SideMenu/>
+        <div className="main"><ul>{ItemBoxList}</ul></div>
+      </div>
+      </div>
+    );
+  }
+  
+  
 }
 
-export default App;
--- src/components/ItemBox.js
+++ src/components/ItemBox.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import "../App.css";
-
+class ItemDropdown extends React.Component {
+    render() {
+        return (
+            <div className="ItemDropdown">
+                <button>&#9660; Action</button>
+                <div className='dropDownContent'>
+                    <span>Delete</span>
+                    <span>Modify</span>
+                </div>
+            </div>
+        );
+    }
+}
 export default class ItemBox extends React.Component {
     render() {
-        
-        const pic =  require(this.props.imgPath);
+   
         return (
         <div className="itemBox">
-            <div classNme = "itemImage">
-                <img src={pic} alt="" style={{height: '80%', width: '20%', aspectRatio: 3/2}}></img>
+            <div className = "itemImage">
+                <img src={this.props.imgPath} alt="" style={{height: '80%', width: '40%', aspectRatio: 3/2}}></img>
             </div>
             <div className='itemText'>
+                <ItemDropdown/>
                 <p>{"Title: "+this.props.title}</p>
                 <p>{"Price: "+this.props.price}</p>
                 <p>{"Category: "+this.props.category}</p>
