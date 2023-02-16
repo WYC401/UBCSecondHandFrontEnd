faa70015689f4a38737296658c13408589ab71e9
WYC401 <545301030@qq.com>
2023-02-16 00:15:51 -0800

    adjust the form looking and layout

--- src/components/ItemInputPage.js
+++ src/components/ItemInputPage.js
@@ -28,24 +28,36 @@ export default class ItemInputPage extends React.Component {
     }
     //https://reactjs.org/docs/forms.html
     render() {
+        const inputStyle={
+            width: "100%",
+            padding: "8px 0px",
+            display: "inline-block",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+            borderSizing: "boder-box"
+        }
         return (
-            <form >
-                <label>
-                    Title:
-                    <input type='text' className='itemTitleInput' value={this.state.title} onChange={this.handleChange}></input>
-                </label>
-                <label>
-                    Price:
-                    <input type='number' className="itemPriceInput" value={this.state.price} onChange={this.handleChange}></input>
-                </label>
-                <label>
-                    Category:
-                    <input type='text' className="itemCategoryInput" value={this.state.category} onChange={this.handleChange}></input>
-                </label>
-                <label>
-                    Description:
-                    <input type='text' className='itemDescrpitionInput' value={this.state.description} onChange={this.handleChange}></input>
-                </label>
+            //https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
+            <form className='itemInputForm'>
+                <div id='title' style={{display: "block"}}>
+                    <label for='title'>Title:</label>
+                    <input type='text' className='itemTitleInput' id='title' value={this.state.title} onChange={this.handleChange} style={inputStyle}></input>
+
+                </div>
+                <div id='price' style={{display: "block"}}>
+                    <label for='price'>Price:</label>
+                    <input type='number' className="itemPriceInput" id='price' value={this.state.price} onChange={this.handleChange} style={inputStyle}></input>
+                </div>
+                
+                <div id='category' style={{display: "block"}}>
+                    <label for='category'>Category:</label>
+                    <input type='text' className="itemCategoryInput" id='category' value={this.state.category} onChange={this.handleChange} style={inputStyle}></input>
+                </div>
+                <div id='description' style={{display: "block"}}>
+                    <label for='description'>Description:</label>
+                    <input type='text' className='itemDescrpitionInput' id='description' value={this.state.description} onChange={this.handleChange} style={inputStyle}></input>
+                </div>
+                <input type="submit" value="Submit"/>
             </form>
         );
     }
